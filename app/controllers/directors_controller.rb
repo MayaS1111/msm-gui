3eb896459908ac810e87250fe7d5be5d86d7d0e9
Maya Sheriff <113066754+MayaS1111@users.noreply.github.com>
2024-06-14 16:03:12 +0000

    started updating director index page

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -36,4 +36,47 @@ class DirectorsController < ApplicationController
 
     render({ :template => "director_templates/eldest" })
   end
+
+  
+  def create
+    d = Director.new
+    d.title = params.fetch("the_title")
+    d.year = params.fetch("the_year")
+    d.duration = params.fetch("the_duration")
+    d.description = params.fetch("the_description")
+    d.image = parads.fetch("the_image")
+    d.director_id = params.fetch("the_director_id")
+
+    d.save
+
+    redirect_to("/directors")
+
+  end
+
+  def destroy
+    
+    the_id = params.fetch("an_id")
+    director = Director.where({:id => the_id}).at(0)
+
+    director.destroy
+
+    redirect_to("/directors")
+
+  end
+
+  def update
+    the_id = params.fetch("an_id")
+    d = Director.where({:id => the_id}).at(0)
+
+    d.title = params.fetch("query_title")
+    d.year = params.fetch("query_year")
+    d.duration = params.fetch("query_duration")
+    d.description = params.fetch("query_description")
+    d.image = params.fetch("query_image")
+    d.director_id = params.fetch("query_director_id")
+
+    d.save
+
+    redirect_to("/directors/#{the_id}")
+  end
 end

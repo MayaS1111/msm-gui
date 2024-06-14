70cb801a515275898056a8d49033ec6627de0ccc
Maya Sheriff <113066754+MayaS1111@users.noreply.github.com>
2024-06-14 15:47:50 +0000

    changes made

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -32,9 +32,28 @@ class MoviesController < ApplicationController
 
   def destroy
     
-    m.save
+    the_id = params.fetch("an_id")
+    movie = Movie.where({:id => the_id}).at(0)
+
+    movie.destroy
 
     redirect_to("/movies")
 
+  end
+
+  def update
+    the_id = params.fetch("an_id")
+    m = Movie.where({:id => the_id}).at(0)
+
+    m.title = params.fetch("query_title")
+    m.year = params.fetch("query_year")
+    m.duration = params.fetch("query_duration")
+    m.description = params.fetch("query_description")
+    m.image = params.fetch("query_image")
+    m.director_id = params.fetch("query_director_id")
+
+    m.save
+
+    redirect_to("/movies/#{the_id}")
   end
 end

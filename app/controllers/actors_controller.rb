38631fc5f4df0d39480bda0ef825fbaf2702350d
Maya Sheriff <113066754+MayaS1111@users.noreply.github.com>
2024-06-14 17:00:54 +0000

    Done!!

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -14,4 +14,42 @@ class ActorsController < ApplicationController
       
     render({ :template => "actor_templates/show" })
   end
+
+  def create
+    a = Actor.new
+    a.name = params.fetch("the_name")
+    a.dob = params.fetch("the_dob")
+    a.bio = params.fetch("the_bio")
+    a.image = params.fetch("the_image")
+
+    a.save
+
+    redirect_to("/actors")
+
+  end
+
+  def destroy
+    
+    the_id = params.fetch("an_id")
+    actor = Actor.where({:id => the_id}).at(0)
+
+    actor.destroy
+
+    redirect_to("/actors")
+
+  end
+
+  def update
+    the_id = params.fetch("an_id")
+    a = Actor.where({:id => the_id}).at(0)
+
+    a.name = params.fetch("query_name")
+    a.dob = params.fetch("query_dob")
+    a.bio = params.fetch("query_bio")
+    a.image = params.fetch("query_image")
+
+    a.save
+
+    redirect_to("/actors/#{the_id}")
+  end
 end
